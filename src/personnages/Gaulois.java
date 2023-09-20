7413a80a4b0bbe56df45d22b1ba01cc06468ce30
Gamberge <cedric.righi@gmail.com>
2023-09-20 18:58:52 +0200

    v1

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,42 @@
+package personnages;
+
+public class Gaulois {
+	private int force;
+	private String nom;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+	
+	
+	 @Override public String toString() { return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]"; }
+	 
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois ("Astérix",8);
+		Romain minus = new Romain ("Minus", 2);
+		System.out.println(asterix);
+		System.out.println(asterix.prendreParole());
+		asterix.parler("Bonjour");
+		asterix.frapper(minus);
+	}
+}
+
